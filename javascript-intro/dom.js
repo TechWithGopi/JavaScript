d3c5f0fe20ce76d211a69f98cce3dd5966edbe1d
GopiKrishna <137504326+TechWithGopi@users.noreply.github.com>
2024-01-03 21:08:32 +0530

    Add files via upload

---
+++ javascript-intro/dom.js
@@ -0,0 +1,31 @@
+// Create a function using manipulateStyles() // 
+// And write the manipulateStyles() in HTML's button element using onclick() event // 
+// create a DOM method "getElementById()" and put in HTML's element where you want// 
+// give a ID name for that and use the ID name in Javascript using a method as document.getElementById() // 
+// As same as above rules, we can styling using .style="" // 
+// And Actionalbe property like textContent to change something on web // 
+
+function manipulateStyles() {
+    document.getElementById("headingElement").textContent = "The World Cricket";
+    document.getElementById("headingElement").style.color = "Grey";
+}
+
+// DOM and EVENT fundamentals // 
+// DOM means Document Object Model // 
+//DOM is the (Structured representation) of the content in the documument created by the browser // 
+// DOM methods - getElementById()
+// Events - onclick Event 
+
+// ** HTML DOM(DOM TREE) ** - Branch out like Tree// 
+// It's called DOM TREE // 
+// It allows Javascipt to change the document structure, style and content. //
+
+// ** NODES AND LINKS ** // 
+// Each node referred to as an "Object" // 
+// Boxes - nodes | Arrows - Links 
+
+// <!DOCTYPE html> = (DOCUMENT OBJECT) It is entry point of the DOM // 
+
+
+
+
---
+++ javascript-intro/variable.js
@@ -0,0 +1,18 @@
+
+// 1) //
+
+let message;
+console.log(message = "Hello World!");
+// we can create variable using assigning operator "=" and let keyword //
+// Assigned a string into a variable named message//  
+// print the string using console.log(); // 
+// variable is used to store informations // 
+// Variable is like a bottle stores water // 
+// we can use variable by many ways // 
+
+// 2) // 
+
+let command = "Command Mode Activated"; 
+console.log(command);
+
+// The above one is just an another way to execute the code //
